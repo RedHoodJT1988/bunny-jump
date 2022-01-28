f401576e01252ca5202e9cf162ef733bdcb64f4f
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-01-28 08:43:24 -0600

    added game over text

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -9,13 +9,14 @@ export default class GameOver extends Phaser.Scene
 
 	create()
 	{
+        const message = `Game Over: Press SPACE`;
 		const width = this.scale.width
 		const height = this.scale.height
 
-		this.add.text(width * 0.5, height * 0.5, 'Game Over', {
-			fontSize: 48
-		})
-		.setOrigin(0.5);
+		this.add.text(width * 0.5, height * 0.5, message, {
+			fontSize: 30
+		}).setOrigin(0.5)
+		
 
 		this.input.keyboard.once('keydown-SPACE', () => {
 			this.scene.start('game')
